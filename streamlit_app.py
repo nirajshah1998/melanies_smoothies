c1d768926134699230d007ca739eb1d866035d95
Niraj Shah <97336282+nirajshah1998@users.noreply.github.com>
2024-09-24 17:32:44 +0530

    added a code with loc

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,9 +27,14 @@ cnx = st.connection("snowflake")
 session = cnx.session()
 
 
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
 # st.dataframe(data=my_dataframe, use_container_width=True)
+# st.stop()
 
+
+pd_df = my_dataframe.to_pandas()
+st.dataframe(pd_df)
+st.stop()
 
 
 ingredients_list = st.multiselect(
@@ -45,6 +50,10 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ', '
+
+        search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen, ' is ', search_on, '.')
+        
         st.subheader(fruit_chosen + 'Nutritionn Information')
         fruit = "https://fruityvice.com/api/fruit/" + str(fruit_chosen)
         fruityvice_response = requests.get(fruit)
